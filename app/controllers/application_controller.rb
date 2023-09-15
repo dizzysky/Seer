3d6789efe618450b81252f5354ad988bb54b41dd
Sanjid <dsanjid@gmail.com>
2023-09-15 16:14:11 -0400

    configured routes; worked on users and sessions controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,46 @@ class ApplicationController < ActionController::API
 
     include ActionController::RequestForgeryProtection
 
-    protect_from_forgery with: :exception
+    # protect_from_forgery with: :exception
     before_action :snake_case_params
-    before_action :attach_authenticity_token
+    # before_action :attach_authenticity_token
+    # skip_before_action :verify_authenticity_token, only: [:test]
+
+    def current_user
+        @current_user ||= User.find_by(session_token: session[:session_token])
+    end
+
+    def login!(user)
+        session[:session_token] = user.reset_session_token!
+    end
+
+    def logout!
+        current_user.reset_session_token! if current_user
+        session[:session_token] = nil
+        @current_user = nil
+    end
+
+    def require_logged_in
+        unless current_user
+            render json: { message: 'Unauthorized' }, status: :unauthorized 
+        end
+    end
+
+
+    def test
+        if params.has_key?(:login)
+            login!(User.first) 
+        elsif params.has_key?(:logout)
+            logout! 
+        end
+
+        if current_user 
+            render json: {user: current_user.slice('id', 'username', 'session_token')}
+        else
+            render json: ['No current user']
+        end
+    end
+
 
     private
     def snake_case_params 
@@ -13,7 +50,7 @@ class ApplicationController < ActionController::API
 
 
     def attach_authenticity_token
-        headers['X-CSRF-Token'] = masked_authenticty_token(session)
+        headers['X-CSRF-Token'] = masked_authenticity_token(session)
     end
     
 end

4d5efe016fa6303f9188922cc7e35de7ff181587
Sanjid <dsanjid@gmail.com>
2023-09-17 15:56:29 -0400

    initial setup frontend auth

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
+import configureStore from './store';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -9,3 +12,21 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+
+
+const store = configureStore(); 
+
+if (process.env.NODE_ENV !== 'production') {
+  window.store = store;
+}
+
+function Root() {
+  return (
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  );
+}

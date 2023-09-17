b4ac9d7d8d61e3dd04963519010960a650dac7ba
Sanjid <dsanjid@gmail.com>
2023-09-17 16:54:31 -0400

    finished frontend auth setup

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,6 +5,17 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import configureStore from './store';
+import csrfFetch, { restoreCSRF } from './store/csrf';
+
+function Root() {
+  return (
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  );
+}
 
 ReactDOM.render(
   <React.StrictMode>
@@ -19,14 +30,20 @@ const store = configureStore();
 
 if (process.env.NODE_ENV !== 'production') {
   window.store = store;
+  window.csrfFetch = csrfFetch
 }
 
-function Root() {
-  return (
-    <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provider>
+const renderApplication = () => {
+  ReactDOM.render(
+    <React.StrictMode>
+      <Root />
+    </React.StrictMode>,
+    document.getElementById('root')
   );
+}
+
+if (sessionStorage.getItem("X-CSRF-Token") === null) {
+  restoreCSRF().then(renderApplication);
+} else {
+  renderApplication();
 }

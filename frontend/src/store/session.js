50f2182d7f46ee7e83120f33fcd5b65f605dd2c5
Sanjid <dsanjid@gmail.com>
2023-09-18 11:50:46 -0400

    added signup functionality

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -33,6 +33,23 @@ export const login = ({ credential, password }) => async dispatch => {
     dispatch(setCurrentUser(data.user));
     return response;
   };
+
+
+export const signup = (user) => async (dispatch) => {
+  const { username, email, password } = user;
+  const response = await csrfFetch("/api/users", {
+    method: "POST", 
+    body: JSON.stringify({
+      username,
+      email,
+      password
+    })
+  });
+  const data = await response.json(); 
+  storeCurrentUser(data.user); 
+  dispatch(setCurrentUser(data.user));
+  return response;
+}
   
 
 export const restoreSession = () => async dispatch => {

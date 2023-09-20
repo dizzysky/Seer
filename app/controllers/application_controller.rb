9c2552a2faf99ee2a3a86f43ce41e4961b253c6d
Sanjid <dsanjid@gmail.com>
2023-09-20 09:14:01 -0400

    added demo user button

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,10 @@
 class ApplicationController < ActionController::API
 
+    include ActionController::RequestForgeryProtection
 
     rescue_from StandardError, with: :unhandled_error
     rescue_from ActionController::InvalidAuthenticityToken,
     with: :invalid_authenticity_token
-
-
-    include ActionController::RequestForgeryProtection
 
     protect_from_forgery with: :exception
     before_action :snake_case_params
@@ -33,22 +31,6 @@ class ApplicationController < ActionController::API
         end
     end
 
-
-    # def test
-    #     if params.has_key?(:login)
-    #         login!(User.first) 
-    #     elsif params.has_key?(:logout)
-    #         logout! 
-    #     end
-
-    #     if current_user 
-    #         render json: {user: current_user.slice('id', 'username', 'session_token')}
-    #     else
-    #         render json: ['No current user']
-    #     end
-    # end
-
-
     private
     def snake_case_params 
         params.deep_transform_keys!(&:underscore)
@@ -61,19 +43,19 @@ class ApplicationController < ActionController::API
 
     def invalid_authenticity_token
         render json: { message: 'Invalid authenticity token' }, 
-          status: :unprocessable_entity
-      end
-      
-      def unhandled_error(error)
+        status: :unprocessable_entity
+    end
+    
+    def unhandled_error(error)
         if request.accepts.first.html?
-          raise error
+            raise error
         else
-          @message = "#{error.class} - #{error.message}"
-          @stack = Rails::BacktraceCleaner.new.clean(error.backtrace)
-          render 'api/errors/internal_server_error', status: :internal_server_error
-          
-          logger.error "\n#{@message}:\n\t#{@stack.join("\n\t")}\n"
+            @message = "#{error.class} - #{error.message}"
+            @stack = Rails::BacktraceCleaner.new.clean(error.backtrace)
+            render 'api/errors/internal_server_error', status: :internal_server_error
+            
+            logger.error "\n#{@message}:\n\t#{@stack.join("\n\t")}\n"
         end
-      end
+    end
     
 end
--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -2,13 +2,24 @@
 
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import ProfileButton from './ProfileButton';
 import './Navigation.css';
 import logo from '../../assets/flickr.png';
+import * as sessionActions from "../../store/session";
+
 
 function Navigation() {
   const sessionUser = useSelector(state => state.session.user);
+  const dispatch = useDispatch();
+
+  const handleDemoLogin = async () => {
+    console.log("we in here");
+    // Make an API call to log in the demo user here, then dispatch the returned data
+    const demoUserData = { credential: 'demo@user.io', password: 'password' };
+    console.log(demoUserData);
+    dispatch(sessionActions.login(demoUserData)); // Replace with your login action
+  };
 
   let sessionLinks;
   if (sessionUser) {
@@ -21,6 +32,7 @@ function Navigation() {
   } else {
     sessionLinks = (
         <>
+          <button className="nav-link" onClick={handleDemoLogin}>Demo User</button>
           <NavLink className="nav-link" to="/login">Log In</NavLink>
           <NavLink className="nav-link" to="/signup">Sign Up</NavLink>
         </>

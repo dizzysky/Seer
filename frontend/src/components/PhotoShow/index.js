f8fd0469cd2310cc0043c764e276855a2ef233bd
Sanjid <dsanjid@gmail.com>
2023-09-27 12:55:25 -0400

    have delete button, but no work

--- frontend/src/components/PhotoShow/index.js
+++ frontend/src/components/PhotoShow/index.js
@@ -15,7 +15,6 @@ const PhotoShow = () => {
   const photo = useSelector(state => state.photos[id]);
   const photoIds = useSelector(state => Object.keys(state.photos));
   const sessionUser = useSelector((state) => state.session.user);
-  console.log("photo ids: ", photoIds);
 
   const currentIndex = photoIds.indexOf(id);
   const nextPhotoId = photoIds[currentIndex + 1];
@@ -37,7 +36,8 @@ const PhotoShow = () => {
 
   const handleDelete = async () => {
     try {
-      await dispatch(deletePhoto(photo.id));
+      const result = await dispatch(deletePhoto(photo.id));
+      console.log('Delete Result:', result);
       history.push('/photos')
     } catch (error) {
       console.error('Delete error: ', error);
@@ -45,14 +45,14 @@ const PhotoShow = () => {
   };
 
   const uploadTime = photo.createdAt ? new Date(photo.createdAt).toLocaleString() : 'Unknown';
-  console.log('Current:', id, 'Prev:', prevPhotoId, 'Next:', nextPhotoId);
+
+
+  console.log('Session User:', sessionUser);
+  console.log('Photo:', photo);
 
   return (
     <div>
-      {/* Render the delete button for the uploader */}
-      {sessionUser && sessionUser.id === photo.userId && (
-        <button onClick={handleDelete} className="delete-button">Delete Photo</button>
-      )}
+     
       <div className="grey-area">
         <button className="arrow-button left" onClick={() => navigateToPhoto(prevPhotoId)}>
           <FontAwesomeIcon icon={faChevronLeft} className="arrow-icon" />
@@ -67,6 +67,12 @@ const PhotoShow = () => {
         <p>Uploaded by: {photo.username || 'Loading...'}</p>
         <p>Uploaded at: {uploadTime}</p>
       </div>
+      {/* Show delete button only if session user is the uploader */}
+      {sessionUser && sessionUser.id === photo.uploaderId && (
+        <button onClick={handleDelete} className="delete-button">
+          Delete Photo
+        </button>
+      )}
     </div>
   );
 };

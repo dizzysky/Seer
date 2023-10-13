0b874ecd9c8dae98908a23a02a1993360da5166a
Sanjid Dewan <dsanjid@gmail.com>
2023-10-13 09:38:29 -0400

    ready to deploy to Render

--- frontend/src/components/CommentItem/index.js
+++ frontend/src/components/CommentItem/index.js
@@ -1,27 +1,26 @@
-import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import * as commentActions from '../../store/comments';
-import 'font-awesome/css/font-awesome.min.css';
-import './CommentItem.css';
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import * as commentActions from "../../store/comments";
+import "font-awesome/css/font-awesome.min.css";
+import "./CommentItem.css";
 
 const CommentItem = ({ comment }) => {
-  console.log(comment);
-  const dispatch = useDispatch();
-  const currentUser = useSelector((state) => state.session.user);
+    const dispatch = useDispatch();
+    const currentUser = useSelector((state) => state.session.user);
 
-  const handleDelete = () => {
-    dispatch(commentActions.removeComment(comment.id));
-  };
+    const handleDelete = () => {
+        dispatch(commentActions.removeComment(comment.id));
+    };
 
-  return (
-    <div className="comment-item">
-       {/* <strong>{comment.author_username ? comment.author_username : "user"}</strong>: {comment.body} */}
-       <p>{comment.body}</p>
-      <button onClick={handleDelete}>
-      <i class="fa-solid fa-trash"></i>
-      </button>
-    </div>
-  );
+    return (
+        <div className="comment-item">
+            {/* <strong>{comment.author_username ? comment.author_username : "user"}</strong>: {comment.body} */}
+            <p>{comment.body}</p>
+            <button onClick={handleDelete}>
+                <i class="fa-solid fa-trash"></i>
+            </button>
+        </div>
+    );
 };
 
 export default CommentItem;

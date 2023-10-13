0843af499ff1542a3a08fc36e9d5140c95e2b8fb
Sanjid Dewan <dsanjid@gmail.com>
2023-10-13 09:37:47 -0400

    completed render preliminaries

--- frontend/src/store/photos.js
+++ frontend/src/store/photos.js
@@ -1,9 +1,9 @@
 import csrfFetch from "./csrf";
 
-const LOAD_PHOTOS = 'photos/LOAD_PHOTOS';
-const RECEIVE_PHOTO = 'photos/RECEIVE_PHOTO';
-const UPLOAD_PHOTO = 'photos/UPLOAD_PHOTO';
-const DELETE_PHOTO = 'photos/DELETE_PHOTO';
+const LOAD_PHOTOS = "photos/LOAD_PHOTOS";
+const RECEIVE_PHOTO = "photos/RECEIVE_PHOTO";
+const UPLOAD_PHOTO = "photos/UPLOAD_PHOTO";
+const DELETE_PHOTO = "photos/DELETE_PHOTO";
 
 export const loadPhotos = (photos) => ({
     type: LOAD_PHOTOS,
@@ -12,9 +12,8 @@ export const loadPhotos = (photos) => ({
 
 export const receivePhoto = (photo) => ({
     type: RECEIVE_PHOTO,
-    photo
+    photo,
 });
-
 
 export const uploadPhoto = (photo) => ({
     type: UPLOAD_PHOTO,
@@ -23,104 +22,87 @@ export const uploadPhoto = (photo) => ({
 
 export const getPhoto = (photoId) => (state) => {
     return state.photos.photoId ? state.photos.photoId : [];
-}
-
+};
 
 export const deletePhoto = (photoId) => ({
-  type: DELETE_PHOTO, 
-  photoId,
-})
-
+    type: DELETE_PHOTO,
+    photoId,
+});
 
 export const createPhoto = (formData) => async (dispatch) => {
     try {
-      const res = await csrfFetch('http://localhost:3000/api/photos', {
-        method: 'POST',
-        body: formData,
-      });
-  
-      if (res.ok) {
-        const photo = await res.json();
-        dispatch(uploadPhoto(photo));
-        return photo;
-      } else if (res.status === 204) { // Fixed typo here from 'res.stats' to 'res.status'
-        console.log("Received 204, no content");
-      } else {
-        try {
-          const data = await res.json();
-          console.log("Upload error: ", data);
-        } catch (e) {
-          console.log("Upload error: syntax error");
+        const res = await csrfFetch("http://localhost:3000/api/photos", {
+            method: "POST",
+            body: formData,
+        });
+
+        if (res.ok) {
+            const photo = await res.json();
+            dispatch(uploadPhoto(photo));
+            return photo;
+        } else if (res.status === 204) {
+            // Fixed typo here from 'res.stats' to 'res.status'
+        } else {
+            try {
+                const data = await res.json();
+            } catch (e) {}
         }
-      }
-    } catch (e) {
-      console.log("Upload Error: ", e);
-    }
-  };
-  
-  
+    } catch (e) {}
+};
 
-
-
-export const fetchPhotos = () => async dispatch => {
-    const res = await csrfFetch('/api/photos');
+export const fetchPhotos = () => async (dispatch) => {
+    const res = await csrfFetch("/api/photos");
     const data = await res.json();
     dispatch(loadPhotos(data));
 };
 
-export const fetchPhoto = (id) => async dispatch => {
+export const fetchPhoto = (id) => async (dispatch) => {
     const res = await csrfFetch(`/api/photos/${id}`);
     const data = await res.json();
     dispatch(receivePhoto(data));
-}
-
-
-export const removePhoto = (photoId) => async dispatch => {
-  console.log(`Attempting to delete photo with id: ${photoId}`);
-  const res = await csrfFetch(`/api/photos/${photoId}`, {
-    method: 'DELETE',
-  });
-  console.log('Delete response:', res);
-  if (res.ok) {
-    const data = await res.json();
-    if (data.id) {
-      dispatch(deletePhoto(data.id));
-    }
-  } else {
-    // error handling
-  }
 };
 
+export const removePhoto = (photoId) => async (dispatch) => {
+    const res = await csrfFetch(`/api/photos/${photoId}`, {
+        method: "DELETE",
+    });
+    if (res.ok) {
+        const data = await res.json();
+        if (data.id) {
+            dispatch(deletePhoto(data.id));
+        }
+    } else {
+        // error handling
+    }
+};
 
-export const updatePhotoCaption = (photoId, newCaption) => async dispatch => {
-  const res = await csrfFetch(`/api/photos/${photoId}`, {
-    method: 'PUT',
-    body: JSON.stringify({caption: newCaption}),
-  });
+export const updatePhotoCaption = (photoId, newCaption) => async (dispatch) => {
+    const res = await csrfFetch(`/api/photos/${photoId}`, {
+        method: "PUT",
+        body: JSON.stringify({ caption: newCaption }),
+    });
 
-  if (res.ok) {
-    const updatedPhoto = await res.json(); 
+    if (res.ok) {
+        const updatedPhoto = await res.json();
 
-    dispatch(receivePhoto(updatedPhoto));
-    window.location.reload();
-  }
-}
-
-
+        dispatch(receivePhoto(updatedPhoto));
+        window.location.reload();
+    }
+};
 
 const photosReducer = (state = {}, action) => {
     switch (action.type) {
-        case LOAD_PHOTOS: 
+        case LOAD_PHOTOS:
             return { ...state, ...action.payload };
         case RECEIVE_PHOTO:
             return { ...state, [action.photo.id]: action.photo };
-        case UPLOAD_PHOTO: 
+        case UPLOAD_PHOTO:
             return { ...state, [action.photo.id]: action.photo };
-        case DELETE_PHOTO: 
-          const newState = { ...state }; 
-          delete newState[action.photoId]; 
-          return newState;
-        default: 
+        case DELETE_PHOTO:
+            const newState = { ...state };
+            delete newState[action.photoId];
+            return newState;
+        default:
             return state;
     }
 };

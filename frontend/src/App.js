14361dc11171b80ab01510d4801d21c02962332b
Sanjid Dewan <dsanjid@gmail.com>
2023-10-13 11:03:22 -0400

    fixed splash scroll

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,47 +1,52 @@
 // frontend/src/App.js
-import React from 'react';
-import { Route, Switch, useLocation } from 'react-router-dom';
+import React from "react";
+import { Route, Switch, useLocation } from "react-router-dom";
 import LoginFormPage from "./components/LoginFormPage";
-import SignupFormPage from './components/SignupFormPage';
-import Navigation from './components/Navigation';
-import SplashPage from './components/SplashPage';
-import PhotosPage from './components/PhotosPage';
-import PhotoShow from './components/PhotoShow';
-import Footer from './components/Footer'
-import PhotoUpload from './components/PhotoUpload';
+import SignupFormPage from "./components/SignupFormPage";
+import Navigation from "./components/Navigation";
+import SplashPage from "./components/SplashPage";
+import PhotosPage from "./components/PhotosPage";
+import PhotoShow from "./components/PhotoShow";
+import Footer from "./components/Footer";
+import PhotoUpload from "./components/PhotoUpload";
 
 function App() {
-  const location = useLocation();
-  const currentPath = location.pathname;
-  return (
-    <div className="main-container">
-      <Navigation />
-      <div className="content">
-        <Switch>
-          <Route exact path="/">
-            <SplashPage />
-          </Route>
-          <Route path="/login">
-            <LoginFormPage />
-          </Route>
-          <Route path="/signup">
-            <SignupFormPage/>
-          </Route>
-          <Route path="/photos/:id">
-            <PhotoShow />
-          </Route>
-          <Route path="/photos">
-            <PhotosPage />
-          </Route>
-          <Route path="/upload">
-            <PhotoUpload />
-          </Route>
-        </Switch>
-      </div>
-      {(currentPath !== '/login' && currentPath !== '/signup') && <Footer className="footer" />}
-    </div>
-  );
+    const location = useLocation();
+    const currentPath = location.pathname;
+    return (
+        <div
+            className={`main-container ${
+                currentPath === "/" ? "no-scroll" : ""
+            }`}
+        >
+            <Navigation />
+            <div className="content">
+                <Switch>
+                    <Route exact path="/">
+                        <SplashPage />
+                    </Route>
+                    <Route path="/login">
+                        <LoginFormPage />
+                    </Route>
+                    <Route path="/signup">
+                        <SignupFormPage />
+                    </Route>
+                    <Route path="/photos/:id">
+                        <PhotoShow />
+                    </Route>
+                    <Route path="/photos">
+                        <PhotosPage />
+                    </Route>
+                    <Route path="/upload">
+                        <PhotoUpload />
+                    </Route>
+                </Switch>
+            </div>
+            {currentPath !== "/login" && currentPath !== "/signup" && (
+                <Footer className="footer" />
+            )}
+        </div>
+    );
 }
-
 
 export default App;
--- frontend/src/components/SplashPage/index.js
+++ frontend/src/components/SplashPage/index.js
@@ -1,34 +1,53 @@
 // frontend/src/components/SplashPage/index.js
-import React, { useState, useEffect } from 'react';
-import './SplashPage.css';
+import React, { useState, useEffect } from "react";
+import "./SplashPage.css";
 
 // Dynamically import all images from a folder
-const imageContext = require.context('../../assets/splash', false, /\.(jpg|jpeg|png)$/);
+const imageContext = require.context(
+    "../../assets/splash",
+    false,
+    /\.(jpg|jpeg|png)$/
+);
 
 const images = imageContext.keys().map(imageContext);
 
 const SplashPage = () => {
-  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+    const [currentImageIndex, setCurrentImageIndex] = useState(0);
 
-  useEffect(() => {
-    const timer = setInterval(() => {
-      setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
-    }, 5000);
+    // Preload images
+    useEffect(() => {
+        images.forEach((image) => {
+            new Image().src = image;
+        });
+    }, []);
 
-    return () => {
-      clearInterval(timer);
-    };
-  }, []);
+    // Switch images
+    useEffect(() => {
+        const timer = setInterval(() => {
+            setCurrentImageIndex(
+                (prevIndex) => (prevIndex + 1) % images.length
+            );
+        }, 5000);
 
-  return (
-    <div className="splash-container" style={{ backgroundImage: `url(${images[currentImageIndex]})` }}>
-      <div className="splash-text">
-        <h1>Find Your Inspiration</h1>
-        <p>Join the Seer community, home to tens of billions of photos and 2 million groups.</p>
-      </div>
-    </div>
-  );
-  
+        return () => {
+            clearInterval(timer);
+        };
+    }, []);
+
+    return (
+        <div
+            className="splash-container"
+            style={{ backgroundImage: `url(${images[currentImageIndex]})` }}
+        >
+            <div className="splash-text">
+                <h1>Find Your Inspiration</h1>
+                <p>
+                    Join the Seer community, home to tens of billions of photos
+                    and 2 million groups.
+                </p>
+            </div>
+        </div>
+    );
 };
 
 export default SplashPage;

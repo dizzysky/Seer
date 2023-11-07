d3f93ca65d80ab242f7bdd7e25d63c252e5cdee8
Sanjid Dewan <dsanjid@gmail.com>
2023-11-07 14:18:30 -0500

    posterity

--- frontend/src/components/TagShow/index.js
+++ frontend/src/components/TagShow/index.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
+import PhotoItem from "../Photos/PhotoItem";
 
 function TagShow() {
     const { tagId } = useParams(); // This will get the `tagId` from the URL
@@ -19,12 +21,18 @@ function TagShow() {
     return (
         <div>
             <h1>Tag: {tag.name}</h1>
-            <div>
+            <div className="grid">
                 {tag.photos.map((photo) => (
-                    <div key={photo.id}>
-                        <img src={photo.url} alt={photo.caption} />
-                        <p>{photo.caption}</p>
-                    </div>
+                    // <div key={photo.id}>
+                    //     <img src={photo.url} alt={photo.caption} />
+                    //     <p>{photo.caption}</p>
+                    // </div>
+                    <Link to={`/photos/${photo.id}`} key={photo.id}>
+                        <PhotoItem
+                            key={photo.id}
+                            photo={{ ...photo, photoUrl: photo.url }}
+                        />
+                    </Link>
                 ))}
             </div>
         </div>

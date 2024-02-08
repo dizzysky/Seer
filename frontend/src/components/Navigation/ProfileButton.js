05581262526b63bd4cefc72ccb104b77871bca71
Sanjid Dewan <dsanjid@gmail.com>
2024-02-08 18:03:18 -0500

    remove unneeded import

--- frontend/src/components/Navigation/ProfileButton.js
+++ frontend/src/components/Navigation/ProfileButton.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { useHistory } from "react-router-dom";
 import * as sessionActions from "../../store/session";
 import "./ProfileButtonStyle.css";

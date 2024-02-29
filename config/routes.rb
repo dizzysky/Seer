159abf2b94161e267f834a537f9c01d454b9f55e
Sanjid Dewan <dsanjid@gmail.com>
2024-02-29 11:12:06 -0500

    refactor routes to incorporate search without confounding photos#show

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,20 @@
 Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
-    resources :users, only: [:create, :show]
-    resource :session, only: [:show, :create, :destroy]
+    # Define specific routes before the more general resource routes
+    get 'photos/search', to: 'photos#search'
     
-    get 'photos/serach', to: 'photos#search'
-
     resources :photos, only: [:show, :index, :create, :edit, :update, :destroy] do
       resources :comments, only: [:create, :index, :destroy, :update]
-      delete 'tags/:tag_id', to: 'photos#remove_tag' 
+      delete 'tags/:tag_id', to: 'photos#remove_tag'
       post 'add_tag', on: :member
     end
 
-
+    resources :users, only: [:create, :show]
+    resource :session, only: [:show, :create, :destroy]
     resources :albums, only: [:index, :show, :create, :update, :destroy]
-
     resources :tags
- 
   end
-   
+
   get 'photos/show_html/:id', to: 'photos#show_html', as: 'photo_show_html'
   post '/api/photos', to: 'photos#create'
 
